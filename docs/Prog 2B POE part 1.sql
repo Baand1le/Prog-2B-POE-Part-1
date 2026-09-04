e89aca2d710ad15f1c8226317808274d466a90ca
Baand1le <st10487923@rcconnect.edu.za>
2026-09-04 20:00:46 +0200

    Created table userRole and table Event The userRole table will help distinguish between whether a user is a participant or an organizer whilst the Event table will all the details relevant to the events

--- docs/Prog 2B POE part 1.sql
+++ docs/Prog 2B POE part 1.sql
@@ -8,3 +8,26 @@ CREATE TABLE userRole (
 );
 
 SELECT * FROM userRole;
+
+CREATE TABLE Users (
+	userID VARCHAR(20) PRIMARY KEY,
+	roleID INT NOT NULL REFERENCES userRole(roleID),
+	firstName VARCHAR(50),
+	Surname VARCHAR(50),
+	email VARCHAR(100)
+);
+
+SELECT * FROM Users;
+
+CREATE TABLE Event (
+	eventID VARCHAR(20) PRIMARY KEY,
+	organizerID VARCHAR(20) NOT NULL REFERENCES Users(userID),
+	Name VARCHAR(100),
+	Description VARCHAR(255),
+	[Date] DATE,
+	Location VARCHAR(255),
+	Distance VARCHAR(20),
+	eventType VARCHAR(30)
+);
+
+SELECT * FROM Event;
